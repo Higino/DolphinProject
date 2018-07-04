e7776b06528aa3182fbdc558045923a70739f6ac
Higino Silva <higino.silva@farfetch.com>
2018-07-04 12:24:12 +0100

    Add Initial set of unit tests for react components

--- FrontEnd/src/components/App/App.test.js
+++ FrontEnd/src/components/App/App.test.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as TestUtils from 'react-dom/test-utils';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+it('App should exist as a react component', () => {
+  const app = TestUtils.renderIntoDocument(
+    <App />
+  );
+
+  expect(TestUtils.isCompositeComponent(app)).toBeTruthy();
+});
+
+
+
+it('Should render a div with class App and a header with class App-header', () => {
+  const app = TestUtils.renderIntoDocument(
+    <App />
+  );
+
+  let headers = TestUtils.scryRenderedDOMComponentsWithClass(app, 'App-header');
+  let mainDiv = TestUtils.scryRenderedDOMComponentsWithClass(app, 'App');
+
+  expect(mainDiv.length).toBeGreaterThan(0);
+  expect(headers.length).toBeGreaterThan(0);
+
 });
